b96f55541bfd430fc4f13e5c6a9d00de78f5c5dd
Lucas Barrionuevo <lukitaz_lbdp@hotmail.com>
2022-03-11 16:23:46 -0300

    cambios sobre cv

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 import Header from './common/Header';
 import Footer from './common/Footer';
 import Inicio from "./container/Inicio";
-import Cv from "./cv/cv/Index";
 import { Route, Routes } from 'react-router-dom';
-
+import Cv from './cv/cv';
 
 const App = () => {
 
