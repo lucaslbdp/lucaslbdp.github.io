3369a9c18a8be9ec60a8c73e1664a82c8fe81d64
Lucas Barrionuevo <lukitaz_lbdp@hotmail.com>
2022-03-11 15:17:09 -0300

    preparado

--- src/common/Header.js
+++ src/common/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto">
                             <Nav.Link href="/cv">CV</Nav.Link>
-                            <Nav.Link target="_blank noreferrer" href="https://github.com/lucaslbdp/cvBarrionuevo.github.io">Repositorio</Nav.Link>
+                            <Nav.Link target="_blank noreferrer" href="https://github.com/lucaslbdp/lucaslbdp.github.io.git">Repositorio</Nav.Link>
                         </Nav>
                         <Nav className='redes-container'>
                             <ul>
